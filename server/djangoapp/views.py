fc27c2b97711f74129f66a152bf4e99f3157ac03
koomekevo <koome.kevo@gurucrafts.net>
2022-02-09 11:20:59 +0000

    Add dealer_details template

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,15 +31,6 @@ def contact(request):
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
-
-# Create an `about` view to render a static about page
-# def about(request):
-# ...
-
-
-# Create a `contact` view to return a static contact page
-#def contact(request):
-
 # Create a `login_request` view to handle sign in request
 def login_request(request):
     context = {}
